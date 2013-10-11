cd5ec27dfd5a459f8cec500f153f9fc1ed7f60e3
IbrahimSba3i <el-sebaie.ebrahim@aucegypt.edu>
2013-10-11 10:48:46 +0200

    First cpp addition

---
+++ driver.cpp
@@ -0,0 +1,10 @@
+#include"score.h"
+
+using namespace std;
+
+void main()
+{
+	score x(10,3,1);						// Maximum number of players, Maximum characters per playername, true for descending false for ascending
+	x.SetFilePath("datafile.txt");			// string file path
+	x.UpdateScores( "HIMA" , 10000 );		// string playername , int playerscore
+}
