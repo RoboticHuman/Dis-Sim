9da220af662e5c2e69d6109f25387d0f7eab7497
IbrahimSba3i <el-sebaie.ebrahim@aucegypt.edu>
2013-10-11 15:18:00 +0200

    Initial plan

---
+++ DisSim.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include "stdlib.h"
+#include <map>
+//#include <string> // if needed..
+
+
+using namespace std;
+
+#ifndef DIS_SIM_H
+#define DIS_SIM_H
+
+// This is just a cooler way to write the skeleton that the dr. sent..
+
+class DisSim
+{
+private:
+
+	// Variables
+	int regs[32];								// The size of each register is 32 bits (int)
+	char memory[8*1024];						// each element in the memory is 8 bits (1 char) and we use 8 kilo bytes
+	ifstream inFile;							// The binary code input file stream
+	ofstream outFile;							// The Assembly code output file stream
+	map< int , char * > regNames;				// Mapping the register numbers with their names
+	unsigned int current_Instr_Address;			// initial address = 0x00400000 and is incremented by 4 each time
+
+	// Functions
+	void emitError( char * );			// This was on the professors skeleton.. will see what is it for
+	char * decodeInst( unsigned int );	// Takes the binary line from the constructor, extracts the opcode and
+										// sends the binary line to either R,I or J depending on the opcode
+										// it then recieves the translation from either and returns it to the constructor
+										// It also increases the value of (current_Instr_Address) by 4 each time because each line is 4 bytes
+	char * decodeR( unsigned int );		
+	char * decodeI( unsigned int );
+	char * decodeJ( unsigned int );
+
+public:
+	DisSim( char * , char * );			// The constructor recieves the input and output file paths
+										// Opens the files, takes instruction by instruction sends it to decodeInst
+										// Recieves the assembly translation of the instruction and puts it in the out file followed by \n
+	~DisSim();
+};
+
+#endif
