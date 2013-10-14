e7ef70f19daea85b6d638cf3ca95c17c22b35e24
RoboticHuman <donomar@aucegypt.edu>
2013-10-14 10:20:53 +0200

    move pseudo added to ADDU

--- DisSim.cpp
+++ DisSim.cpp
@@ -158,6 +158,10 @@ char* DisSim::decodeR( unsigned int instWord)
 					if( Labels.find(current_Instr_Address) != Labels.end() )
 						strstream<< "label" << Labels.at(current_Instr_Address) << ":";
 				}
+		if(rt == 0)
+		{
+			strstream<< "\tmove\t$" << regNames.find(rd)->second <<",\t$" << regNames.find(rs)->second;
+		}
 		strstream<< "\taddu\t$" << regNames.find(rd)->second <<",\t$" << regNames.find(rs)->second <<",\t$" << regNames.find(rt)->second;
 		}
 		break;
